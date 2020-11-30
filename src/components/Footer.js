eccb59fd71ca03501a43264b3558bd05d017fbd4
SREEHARI <sreehari2610@gmail.com>
2020-11-30 15:39:10 +0530

    header&footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+ <footer className="foot">
+            &copy; Copyright 2020
+        </footer>
+        </div>
+       
+    )
+}
